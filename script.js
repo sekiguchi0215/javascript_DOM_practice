38efe5c295e7f92ef89d66ecb6b412e647fea27f
sekiguchi0215 <dsphere5296@gmail.com>
2021-05-18 01:30:02 +0900

    【課題】新しいボタンを作成し、アラートが表示されるようにする。

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const button = document.getElementById('test-button')
 button.onclick = () => {
   alert('動作確認ができました！')
 }
+
+const new_button = document.getElementById('new-button')
+new_button.onclick = () => {
+  alert('新しいボタンです！')
+}
